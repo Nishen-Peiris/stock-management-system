dd10734cc37e83bf08ff0ab766b4fc1a9fcbee3e
Nishen Peiris <nishenkpeiris@gmail.com>
2017-09-01 15:48:27 +0530

    Active tab gets highlighted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,9 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  name = 'Angular';
+  activeTab: string;
+
+  public setActiveTab(activeTab: string) {
+    this.activeTab = activeTab;
+  }
 }
