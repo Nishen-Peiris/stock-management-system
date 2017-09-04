e8accea72a3bcd762530639c2c6c4ebbcfbc087b
Nishen Peiris <nishenkpeiris@gmail.com>
2017-09-04 20:45:07 +0530

    Configure as a Maven project with Spring Boot

---
+++ src/main/java/com/nishenpeiris/StockManagementSystem/StockManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.nishenpeiris.StockManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementSystemApplication.class, args);
+	}
+}
